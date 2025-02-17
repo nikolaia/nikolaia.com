9b6707e1a995cb4d18abcde5a2df6c9e5f28bb65
nikolaia <nikolai@stasj.com>
2025-02-17 11:58:59 +0100

    fix: redirects

--- astro.config.ts
+++ astro.config.ts
@@ -41,21 +41,21 @@ export default defineConfig({
   experimental: {
     contentLayer: true,
   },
+
   redirects: {
-    // old urls to new urls for SEO
-    "/naar-autonomi-blir-anarki": "/posts/naar-autonomi-blir-anarki",
+    "/naar-autonomi-blir-anarki": "/posts/nar-autonomi-blir-anarki",
     "/tre-ting-du-ma-tenke-pa-for-du-gar-pa-tiktok":
       "/posts/tre-ting-du-ma-tenke-pa-for-du-gar-pa-tiktok",
     "/grunnen-til-at-du-ikke-far-til-smidig-er-fordi-du-fokuserer-pa-feil-ting":
       "/posts/grunnen-til-at-du-ikke-far-til-smidig-er-fordi-du-fokuserer-pa-feil-ting",
     "/ingenting-aa-skjule-det-er-fortsatt-problematisk-at-din-persondata-lagres-sentralt":
-      "/posts/ingenting-aa-skjule-det-er-fortsatt-problematisk-at-din-persondata-lagres-sentralt",
+      "/posts/ingenting-a-skjule-persondata-lagres-sentralt",
     "/how-to-use-elastic-beanstalk-with-an-ip-whitelisted-api-terraform":
       "/posts/static-outgoing-ip-elastic-beanstalk-terraform",
     "/consolidate-nuget-packages-fake-target":
-      "/posts/consolidate-nuget-packages-fake-target",
+      "/posts/ensuring-nuget-packages-consolidated-fake-target",
     "/aspnet5-development-on-osx-with-docker":
-      "/posts/aspnet-5-in-docker-on-osx ",
+      "/posts/aspnet-5-in-docker-on-osx",
     "/octopus-deploy-and-teamcity-integration-in-slack":
       "/posts/teamcity-and-octopus-deploy-in-slack",
     "/the-lean-startup": "/posts/the-lean-startup",
